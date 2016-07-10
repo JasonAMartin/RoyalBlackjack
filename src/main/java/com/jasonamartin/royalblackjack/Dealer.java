a11c2d6537ca29cf4beda086e1c34defe3888be5
Jason A. Martin <jason.martin@jasonamartin.com>
2016-07-09 22:31:18 -0700

    bug: Fixed currentHandValue conditional to be >= 17 so the dealer didn't hit himself to death.

--- src/main/java/com/jasonamartin/royalblackjack/Dealer.java
+++ src/main/java/com/jasonamartin/royalblackjack/Dealer.java
@@ -5,11 +5,12 @@ public class Dealer extends Players {
     public int checkAction (int currentHandValue){
         // action based on hand value. 1 is hit and 2 is stand
         // Dealer hits on 2 - 16 and stands on all 17+
+        System.out.println(String.format("Sending in dealer HV: %d", currentHandValue));
         int actionCode = 0;
         if (currentHandValue >= 2 && currentHandValue <= 16) {
             actionCode = 1;
         }
-        if (currentHandValue >= 17 && currentHandValue <= 21) {
+        if (currentHandValue >= 17) {
             actionCode = 2;
         }
         return actionCode;
