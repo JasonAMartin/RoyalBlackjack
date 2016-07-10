7853d0aedddd43c47fb19c6105fb5410f2a4d515
Jason A. Martin <jason.martin@jasonamartin.com>
2016-07-09 21:24:24 -0700

    refactor: cleaning up the Dealer class by removing the very large case statement and replacing it with two small conditionals.

--- src/main/java/com/jasonamartin/royalblackjack/Dealer.java
+++ src/main/java/com/jasonamartin/royalblackjack/Dealer.java
@@ -2,91 +2,16 @@ package com.jasonamartin.royalblackjack;
 
 public class Dealer extends Players {
 
-
     public int checkAction (int currentHandValue){
-        int actionCode=0;
-
-        //action based on hand value. 1 is hit and 2 is stand
-
-
-        switch (currentHandValue){
-
-            case 2:
-                actionCode=1;
-                break;
-            case 3:
-                actionCode=1;
-                break;
-            case 4:
-                actionCode=1;
-                break;
-            case 5:
-                actionCode=1;
-                break;
-            case 6:
-                actionCode=1;
-                break;
-            case 7:
-                actionCode=1;
-                break;
-            case 8:
-                actionCode=1;
-                break;
-            case 9:
-                actionCode=1;
-                break;
-            case 10:
-                actionCode=1;
-                break;
-            case 11:
-                actionCode=1;
-                break;
-            case 12:
-                actionCode=1;
-                break;
-            case 13:
-                actionCode=1;
-                break;
-            case 14:
-                actionCode=1;
-                break;
-            case 15:
-                actionCode=1;
-                break;
-            case 16:
-                actionCode=1;
-                break;
-            case 17:
-                actionCode=2;
-                break;
-            case 18:
-                actionCode=2;
-                break;
-            case 19:
-                actionCode=2;
-                break;
-            case 20:
-                actionCode=2;
-                break;
-            case 21:
-                actionCode=2;
-                break;
-
-
-
-            default:
-                actionCode=2;
-                break;
-
+        // action based on hand value. 1 is hit and 2 is stand
+        // Dealer hits on 2 - 16 and stands on all 17+
+        int actionCode = 0;
+        if (currentHandValue >= 2 && currentHandValue <= 16) {
+            actionCode = 1;
         }
-
-
-
-
-
+        if (currentHandValue >= 17 && currentHandValue <= 21) {
+            actionCode = 2;
+        }
         return actionCode;
     }
-
-
-
 }
