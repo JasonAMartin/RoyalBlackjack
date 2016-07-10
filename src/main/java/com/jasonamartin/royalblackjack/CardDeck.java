36b9db05c46c1fab018fbe04c335740ec552c37c
Jason A. Martin <jason.martin@jasonamartin.com>
2016-07-09 20:04:23 -0700

    feat: adding GetHandValue method so game can determine a participant's hand. This was previously in the Player's class. Cleaned it up some as well.

--- src/main/java/com/jasonamartin/royalblackjack/CardDeck.java
+++ src/main/java/com/jasonamartin/royalblackjack/CardDeck.java
@@ -56,7 +56,54 @@ public class CardDeck {
         Collections.shuffle(cardShoe);
     }
 
+    static int GetHandValue (String hand) {
+        int handValue = 0;
+        switch (hand.charAt(1)){
+            case '1':
+                handValue+=10;
+                break;
+            case 'j':
+                handValue+=10;
+                break;
+            case 'q':
+                handValue+=10;
+                break;
+            case 'k':
+                handValue+=10;
+                break;
+            case 'a':
+                handValue+=11;
+                break;
+            case '2':
+                handValue+=2;
+                break;
+            case '3':
+                handValue+=3;
+                break;
+            case '4':
+                handValue+=4;
+                break;
+            case '5':
+                handValue+=5;
+                break;
+            case '6':
+                handValue+=6;
+                break;
+            case '7':
+                handValue+=7;
+                break;
+            case '8':
+                handValue+=8;
+                break;
+            case '9':
+                handValue+=9;
+                break;
+            default:
+                break;
 
+        }
+        return handValue;
+    }
 
 
     public void GetCardSuit (char a){
